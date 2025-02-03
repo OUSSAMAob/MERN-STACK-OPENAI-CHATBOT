f4e861f84e1c93662c59e9d8800e17fed85fe7fb
OUSSAMAob <oussamaoubedda14@gmail.com>
2025-02-03 12:46:56 +0100

    final modification

---
+++ backend/dist/index.js
@@ -0,0 +1,12 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+// Connection and listneres
+const PORT = process.env.PORT || 5000;
+connectToDatabase().then(() => {
+    app.listen(PORT, () => console.log("Server Open & Connected To Databasehand 👌"));
+}).catch(err => console.log(err));
+// app.delete("/user/:id", (req, res, next) => {
+//   console.log(req.params.id);
+//   return res.send("Hello");
+// });
+//# sourceMappingURL=index.js.map
